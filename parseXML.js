ca396b0aafc310651529becd833721b3b1a301a0
pweids <paul.weidinger@gmail.com>
2012-09-30 15:58:43 -0400

    wrapped parseXML in jquery ready state

--- parseXML.js
+++ parseXML.js
@@ -1,21 +1,22 @@
-function addLibrary(file) {
-    var xmlhttp= new XMLHttpRequest();
-    xmlhttp.open("GET", file,false);
-    xmlhttp.send();
-    libraryXML=xmlhttp.responseXML;
-    var songNodes = libraryXML.getElementsByTagName('key');
-    var songs = []
-    for (var i = 0; i <songNodes.length; i++) {
+$(document).ready(function() {
+    function addLibrary(file) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", file, false);
+        xmlhttp.send();
+        libraryXML = xmlhttp.responseXML;
+        var songNodes = libraryXML.getElementsByTagName('key');
+        var songs = []
+        for (var i = 0; i < songNodes.length; i++) {
             if (songNodes[i].firstChild.data === "Name") {
                 var song = {
-                    "Song" : songNodes[i].nextSibling,
-                    "Artist": songNodes[i+1].nextSibling,
-                    "Album": songNodes[i+3].nextSibling,
-                    "Time": songNodes[i+7].nextSibling,
+                    "Song": songNodes[i].nextSibling,
+                    "Artist": songNodes[i + 1].nextSibling,
+                    "Album": songNodes[i + 3].nextSibling,
+                    "Time": songNodes[i + 7].nextSibling,
                 }
             }
             songs.push(song);
         }
-    return songs
-}
-console.log(addLibrary("Library.xml"));
+        return songs
+    }
+});
