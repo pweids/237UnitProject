e537c7531f626ff23039134a4c75457d36d6ecda
Jeff <jmich@andrew.cmu.edu>
2012-09-30 14:39:31 -0400

    parse library changes

--- parseXML.js
+++ parseXML.js
@@ -1,9 +1,18 @@
-var libraryXML
-function loadXML{
-	var libraryXML = document.implementation.createDocument("","",null);
-	libraryXML.load("library.xml");
-	libraryXML.onload=parseXML;
-}
-function {
-	console.log(libraryXML);
-}
+xmlhttp=new XMLHttpRequest();
+xmlhttp.open("GET","library.xml",false);
+xmlhttp.send();
+libraryXML=xmlhttp.responseXML;
+var songNodes = libraryXML.getElementsByTagName('key');
+var songs = []
+for (var i = 0; i <songNodes.length; i++) {
+        if (songNodes[i].firstChild.data === "Name") {
+            var song = {
+                "Song" : songNodes[i].nextSibling,
+                "Artist": songNodes[i+1].nextSibling,
+                "Album": songNodes[i+3].nextSibling,
+                "Time": songNodes[i+7].nextSibling,
+            }
+        }
+        songs.push(song);
+    }
+console.log(songs);
