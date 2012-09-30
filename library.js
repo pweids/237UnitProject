f2f2ee7337d263f4d440e1ddd37f0838d37c8bdc
pweids <paul.weidinger@gmail.com>
2012-09-30 18:29:14 -0400

    added comments to JS

--- library.js
+++ library.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+    
+    
+//Helper Functions
+    
+    //This clears the selection on the window to avoid double-click selection when interacting with elems
     function clearSelection() {
         if(document.selection && document.selection.empty) {
             document.selection.empty();
@@ -8,22 +13,30 @@ $(document).ready(function() {
         }
     }
     
+    
+    
+//Click Event Handlers
+    
+    // When the "Playlist" button is hit it shows the listblock
     $(".expander.plist").click(function() {
         $(".plist.listblock").toggle();
         clearSelection();
     });
     
+    //When the browse button is hit, it shows the listblock
     $(".expander.lib").click(function() {
         $(".lib.listblock").toggle();
         clearSelection();
     });
     
+    //When a playlist is selected
     $(".plist li").click(function() {
         $(this).parent().find('li').removeClass('selected');
         $('.lib').removeClass('selected');
         $(this).addClass('selected');
     });
     
+    //when the library is selected
     $(".lib.listblock").click(function() {
         $('.plist ul').find('li').removeClass('selected');
         $(this).addClass('selected');
