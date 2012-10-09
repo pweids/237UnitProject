376ea6d218ada14f98ca4c2bd7feffb228ec762e
pweids <paul.weidinger@gmail.com>
2012-10-08 23:01:02 -0400

    added null playlist validation

--- library.js
+++ library.js
@@ -1,40 +1,46 @@
 $(document).ready(function() {
-    var currentSong = {name:null, artist:null};
+    var currentSong = {
+        name: null,
+        artist: null
+    };
     var currentTR;
     var playlists = [];
-//Helper Functions
-
+    //Helper Functions
     //This makes the top bar width flexible and more accurate
+
+
     function setSizes() {
-       var rightWidth = Math.max($('body').width()-216, 400);
-       $("#topbar").width(rightWidth);
-       $('.songlist').width(rightWidth);
-       $('.songlist > table').width(rightWidth);
+        var rightWidth = Math.max($('body').width() - 216, 400);
+        $("#topbar").width(rightWidth);
+        $('.songlist').width(rightWidth);
+        $('.songlist > table').width(rightWidth);
     }
-    
-    
+
+
     //This clears the selection on the window to avoid double-click selection when interacting with elems
+
+
     function clearSelection() {
-        if(document.selection && document.selection.empty) {
+        if (document.selection && document.selection.empty) {
             document.selection.empty();
-        } else if(window.getSelection) {
+        } else if (window.getSelection) {
             var sel = window.getSelection();
             sel.removeAllRanges();
         }
     }
-    
+
     function getReadableTime(songtime) {
         x = songtime / 1000;
         seconds = Math.round(x % 60);
         x /= 60;
         minutes = Math.round(x % 60);
-        
-        return minutes + ((seconds<10) ? ":0" : ":") + seconds ;
+
+        return minutes + ((seconds < 10) ? ":0" : ":") + seconds;
     }
-    
-//Main Functionality
+
+    //Main Functionality
     setSizes();
-    /*
+/*
     if (!localStorage.getItem('playlists')) {
         localStorage.setItem('playlists', JSON.stingify(playlists));
     }
@@ -42,84 +48,83 @@ $(document).ready(function() {
         playlists = JSON.parse(sessionStorage.getItem("playlists"));
     }
     */
-    
+
     var songs = JSON.parse(sessionStorage.getItem("library"));
     if (!songs) {
         alert("Error! Bad Session Storage");
-    }
-    else {
+    } else {
         $.each(songs, function(key, value) {
-            $('.songlist table').append('<tr><td>' + value.name + 
-            '</td><td>' + getReadableTime(value.time) + '</td><td>' + value.artist + 
-            '</td><td>' + value.album + '</td></tr>');
+            $('.songlist table').append('<tr><td>' + value.name + '</td><td>' + getReadableTime(value.time) + '</td><td>' + value.artist + '</td><td>' + value.album + '</td></tr>');
         });
     }
-        
 
-    
-//Event Handlers
 
-    $(window).resize(function() { setSizes(); });
-    
+
+    //Event Handlers
+    $(window).resize(function() {
+        setSizes();
+    });
+
     // When the "Playlist" button is hit it shows the listblock
     $(".expander.plist").click(function() {
         $(".plist.listblock").toggle();
         clearSelection();
     });
-    
+
     //When the browse button is hit, it shows the listblock
     $(".expander.lib").click(function() {
         $(".lib.listblock").toggle();
         clearSelection();
     });
-    
+
     //When a playlist is selected
-    
     //when the library is selected
     $(".lib.listblock").click(function() {
         $('.plist ul').find('li').removeClass('selected');
         $(this).addClass('selected');
         $('.songlist tr').show();
     });
-    
+
     $('#addPlaylist').click(function() {
         var plistname = prompt("Insert name for new playlist");
-        $('.plist.listblock ul').append('<li>'+plistname+'</li>');
-        $('#addBoxSelections').append($('<option></option>').text(plistname));
-        $(".plist.listblock").toggle();
-        playlists.push(plistname);
-        $(".plist li").click(function() {
-            $(this).parent().find('li').removeClass('selected');
-            $('.lib').removeClass('selected');
-            $(this).addClass('selected');
-            $('.songlist tr').hide();
-            classname = '.playlist-' + $(this).text();
-            $(classname).show();
-        });
+        if (plistname) {
+            $('.plist.listblock ul').append('<li>' + plistname + '</li>');
+            $('#addBoxSelections').append($('<option></option>').text(plistname));
+            $(".plist.listblock").toggle();
+            playlists.push(plistname);
+            
+            $(".plist li").click(function() {
+                $(this).parent().find('li').removeClass('selected');
+                $('.lib').removeClass('selected');
+                $(this).addClass('selected');
+                $('.songlist tr').hide();
+                classname = '.playlist-' + $(this).text();
+                $(classname).show();
+            });
+        }
     });
-    
+
     $(".songlist tr").click(function(e) {
         var name = $(this).children(':nth-child(1)').text();
         var artist = $(this).children(':nth-child(3)').text();
-        $("#addBox").css('left', e.pageX-216).css('top', e.pageY-40).show();
-        currentSong.name=name;
-        currentSong.artist=artist;
+        $("#addBox").css('left', e.pageX - 216).css('top', e.pageY - 40).show();
+        currentSong.name = name;
+        currentSong.artist = artist;
         currentTR = $(this);
     });
-    
-    $("#addBox").mouseleave( function() {
-        if (!$('#addBoxSelections').is(':focus'))
-            $(this).fadeOut()
+
+    $("#addBox").mouseleave(function() {
+        if (!$('#addBoxSelections').is(':focus')) $(this).fadeOut()
         $('.songlist .selected').removeClass('selected');
     })
-    
+
     $('#addBoxSelections').change(function() {
         selectedplist = $((':selected'), $(this)).text();
         currentTR.addClass('playlist-' + selectedplist);
         $(this).blur();
     })
-    
-    $('#playit').click( function() {
+
+    $('#playit').click(function() {
         var searchString = currentSong.name + currentSong.artist;
         getSong(searchString);
     })
